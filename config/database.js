8639bdcd9fcc25fa29cef31b8f29cc27ab1cacfd
SILVA TAMBUNAN <silvaanjelina07@gmail.com>
2022-06-11 23:22:21 +0700

    update config db

--- config/database.js
+++ config/database.js
@@ -1,9 +1,9 @@
 const {
-  DB_USER = "",
-  DB_PASSWORD = "",
-  DB_NAME = "bcr",
-  DB_HOST = "127.0.0.1",
-  DB_PORT = "5432",
+  DB_USER = "postgres",
+    DB_PASSWORD = "silva100%",
+    DB_NAME = "bcr",
+    DB_HOST = "127.0.0.1",
+    DB_PORT = "5432",
 } = process.env;
 
 module.exports = {
